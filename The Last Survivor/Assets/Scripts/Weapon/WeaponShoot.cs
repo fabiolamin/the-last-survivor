4a9e53ec8e17251558e3450bc3fd6689c8964446
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-20 18:11:51 -0300

    Implementing shoot's mechanic

---
+++ The Last Survivor/Assets/Scripts/Weapon/WeaponShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private float speed = 100f;
+  
+    private void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody newBulletRigidbody = newBullet.GetComponent<Rigidbody>();
+            newBulletRigidbody.velocity += transform.forward * speed;
+            Destroy(newBullet,3f);
+        }
+    }
+}
