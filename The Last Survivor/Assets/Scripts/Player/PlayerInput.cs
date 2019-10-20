0a5b3c33fe66b9a2a32ce9a20a77b4f050b26687
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-20 09:23:45 -0300

    Implementing the player's movement

---
+++ The Last Survivor/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector3 GetWASDKeys()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            return transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            return -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            return transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            return -transform.right;
+        }
+
+        return Vector3.zero;
+    }
+
+    public float GetMouseX()
+    {
+        return Input.GetAxis("Mouse X");
+    }
+}
---
+++ The Last Survivor/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    [SerializeField]
+    private float speedMovement = 5f;
+    [SerializeField]
+    private float speedRotation = 2f;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    public void Move()
+    {
+        Vector3 move = Vector3.zero;
+        move += playerInput.GetWASDKeys();
+        transform.position += move * Time.deltaTime * speedMovement;
+    }
+
+    public void Rotate()
+    {
+        float horizontal = playerInput.GetMouseX();
+        Vector3 rotation = new Vector3(0, horizontal, 0) * speedRotation;
+        transform.Rotate(rotation);
+    }
+}
