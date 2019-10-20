9639af83960047aac118d118707efd830dbffa6a
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-20 18:56:29 -0300

    Implementing the enemy's spawn system

---
+++ The Last Survivor/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+    [SerializeField]
+    private float spawnInterval = 1f;
+    private float timerAux;
+
+    private void Awake()
+    {
+        timerAux = spawnInterval;
+    }
+    private void Update()
+    {
+        spawnInterval -= Time.deltaTime;
+        if (spawnInterval < 0)
+        {
+            Spawn();
+            spawnInterval = timerAux;
+        }
+    }
+    void Spawn()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+}
