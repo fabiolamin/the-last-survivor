1100b2c2ee027f64304230970b2671218026146b
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-26 10:39:30 -0300

    Creating Health class

---
+++ The Last Survivor/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Health
+{
+    [SerializeField]
+    private int healthValue = 100;
+    public int HealthValue
+    {
+        get { return healthValue; }
+
+        set
+        {
+            healthValue = value;
+        }
+    }
+
+    void ChangeHealth(int amount)
+    {
+        healthValue += amount;
+    }
+}
