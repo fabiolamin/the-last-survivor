c18481e98e7f9f1d8e1a05809419b757c3d81101
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-26 11:10:44 -0300

    Creating EnemyCollider class

---
+++ The Last Survivor/Assets/Scripts/Enemy/EnemyCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+    Health enemyHealth;
+
+    private void Awake()
+    {
+        enemyHealth = GetComponent<Health>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            enemyHealth.ChangeHealth(-10);
+            if(enemyHealth.HealthValue <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
