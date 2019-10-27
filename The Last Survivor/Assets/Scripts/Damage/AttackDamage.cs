17acdf4030a3d0a92b2bbd4264fe0f613b49bdfa
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-27 10:08:53 -0300

    Creating AttackDamage class

---
+++ The Last Survivor/Assets/Scripts/Damage/AttackDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDamage : MonoBehaviour
+{
+    [SerializeField]
+    private int value = 10;
+    public int Value
+    {
+        get{ return value; }
+        set{ this.value = value; }
+    }
+}
