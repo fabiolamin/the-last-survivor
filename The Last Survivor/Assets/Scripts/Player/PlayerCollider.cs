eaf08b174e78e3f0cda0dcca7ee5226980e3ee69
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-27 09:07:10 -0300

    Creating PlayerCollider class

---
+++ The Last Survivor/Assets/Scripts/Player/PlayerCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    Health playerHealth;
+
+    private void Awake()
+    {
+        playerHealth = GetComponent<Health>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            playerHealth.ChangeHealth(-10);
+            {
+                if (playerHealth.HealthValue <= 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
