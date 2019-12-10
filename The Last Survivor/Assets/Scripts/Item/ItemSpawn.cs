b2611132fb44b49a35a398d79b95b5e8cc370c85
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-10 09:38:28 -0300

    Creating ItemSpawn script

---
+++ The Last Survivor/Assets/Scripts/Item/ItemSpawn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    private float timer;
+    [SerializeField]
+    private GameObject[] items;
+    [SerializeField]
+    private float spawnInterval = 3f;
+    public bool IsActiveToSpawn { get; set; }
+
+    private void Awake()
+    {
+        timer = spawnInterval;
+        IsActiveToSpawn = true;
+    }
+
+    private void Update()
+    {
+        spawnInterval -= Time.deltaTime;
+        if(spawnInterval <= 0)
+        {
+            Spawn();
+            spawnInterval = timer;
+        }
+    }
+
+    private void Spawn()
+    {
+        if(IsActiveToSpawn)
+        {
+            int random = Random.Range(0, items.Length);
+            items[random].SetActive(true);
+        }
+    }
+}
