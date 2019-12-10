f6dea5ae5352e353520852d923aaedb3dacb2662
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-10 08:52:36 -0300

    Creating GameObjectSpawn script

---
+++ The Last Survivor/Assets/Scripts/Spawn/GameObjectSpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectSpawn : MonoBehaviour
+{
+    private float timerAux;
+    private Pool gameObjectSpawnPool;
+    private int position;
+    [SerializeField]
+    private float spawnInterval = 1f;
+
+    public float SpawnInterval
+    {
+        get { return spawnInterval; }
+        set { spawnInterval = value; }
+    }
+
+    private void Awake()
+    {
+        timerAux = spawnInterval;
+        gameObjectSpawnPool = GetComponent<Pool>();
+        position = 0;
+    }
+    private void Update()
+    {
+        timerAux -= Time.deltaTime;
+        if (timerAux < 0)
+        {
+            Spawn();
+            timerAux = spawnInterval;
+        }
+    }
+    private void Spawn()
+    {
+        if (position < gameObjectSpawnPool.InstantiatePrefabs.Length)
+        {
+            gameObjectSpawnPool.EnablePrefab(position);
+            position++;
+        }
+    }
+}
