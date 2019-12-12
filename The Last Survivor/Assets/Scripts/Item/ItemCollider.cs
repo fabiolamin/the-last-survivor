90e6efbc9fe204b08d4207c028eae19c9711a097
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-12 09:32:40 -0300

    Creating ItemCollider script

---
+++ The Last Survivor/Assets/Scripts/Item/ItemCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollider : MonoBehaviour
+{
+    [SerializeField]
+    private ItemSpawn itemSpawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        itemSpawn.IsActiveToSpawn = other.gameObject.CompareTag("Player");
+    }
+}
