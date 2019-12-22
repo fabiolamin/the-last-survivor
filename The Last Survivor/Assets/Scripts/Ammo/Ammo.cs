e1ef5be12725d6b2497d0dfea34017ac0ba63e9d
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-22 11:01:43 -0300

    Creating Ammo script

---
+++ The Last Survivor/Assets/Scripts/Ammo/Ammo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField]
+    private int amount = 50;
+    public int Amount
+    {
+        get{ return amount; }
+        private set{ amount = value; }
+    }
+
+    public void Change(int value)
+    {
+        amount += value;
+    }
+}
