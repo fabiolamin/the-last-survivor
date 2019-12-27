08b041307dfd9f8d906a8d69a5eebec775cb64c3
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-26 21:17:29 -0300

    Creating PlayerAnimation script

---
+++ The Last Survivor/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void AnimateMovement(bool answer)
+    {
+        animator.SetBool("IsRunning", answer);
+    }
+}
