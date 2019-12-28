07cafbd5886bcfd3cf383d2ecd3ea7eed000e1fa
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-28 11:16:00 -0300

    Creating EnemyAnimation script

---
+++ The Last Survivor/Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    private Animator animator;
+    public bool IsAttacking { get; set; }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        IsAttacking = false;
+    }
+
+    private void Update()
+    {
+        animator.SetBool("IsAttacking", IsAttacking);
+    }
+
+}
