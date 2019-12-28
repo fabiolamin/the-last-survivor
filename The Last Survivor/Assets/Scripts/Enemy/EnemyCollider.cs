de5cce5eeb84327be64fbc981e91d52e0d6cef4c
Fábio Lamin <fabio_lamin@hotmail.com>
2019-12-28 11:27:01 -0300

    Implementing enemy's animation conditions based in his collision with the Player

--- The Last Survivor/Assets/Scripts/Enemy/EnemyCollider.cs
+++ The Last Survivor/Assets/Scripts/Enemy/EnemyCollider.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class EnemyCollider : MonoBehaviour
 {
+    private EnemyAnimation enemyAnimation;
     private void Awake()
     {
+        enemyAnimation = GetComponent<EnemyAnimation>();
     }
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            enemyAnimation.IsAttacking = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            enemyAnimation.IsAttacking = false;
+        }
     }
 }
