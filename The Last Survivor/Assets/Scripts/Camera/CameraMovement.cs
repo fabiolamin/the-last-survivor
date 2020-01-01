f756c582e283a3e570bef199cf524ef9dbf3b6f8
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-01 09:52:44 -0300

    Creating CameraMovement script
    The CameraMovement script allow the camera to follow the player.

---
+++ The Last Survivor/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Vector3 offSet;
+
+    private void Awake()
+    {
+        offSet = transform.position - target.position;
+    }
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x + offSet.x, transform.position.y, target.position.z + offSet.z);
+    }
+}
