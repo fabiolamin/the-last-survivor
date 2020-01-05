734af540062ba40c5ea79aa21f93bc66780c2861
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-05 09:08:18 -0300

    Implementing UI at Main scene

---
+++ The Last Survivor/Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButton : MonoBehaviour
+{
+    public bool IsScenePaused { get; private set; }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        IsScenePaused = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        IsScenePaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Resume();
+    }
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ The Last Survivor/Assets/Scripts/UI/UIStatus.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStatus : MonoBehaviour
+{
+    private UIButton UIButton;
+    [SerializeField]
+    private RoundController roundController;
+    [SerializeField]
+    private Health health;
+    [SerializeField]
+    private Ammo ammo;
+    [SerializeField]
+    private Text roundNumberText;
+    [SerializeField]
+    private Text healthAmountText;
+    [SerializeField]
+    private Text ammoAmountText;
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    private void Awake()
+    {
+        UIButton = GetComponent<UIButton>();
+    }
+
+    private void Update()
+    {
+        SetText();
+
+        pauseMenu.SetActive(UIButton.IsScenePaused);
+    }
+
+    private void SetText()
+    {
+        roundNumberText.text = roundController.RoundNumber.ToString();
+        healthAmountText.text = health.Amount.ToString();
+        ammoAmountText.text = ammo.Amount.ToString();
+    }
+}
